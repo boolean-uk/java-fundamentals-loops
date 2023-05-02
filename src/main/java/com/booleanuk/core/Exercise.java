1387ba77a6f3b29b5ed5f01a51d64ff8b0e5f331
Farhang Younis <comp-sci@hotmail.com>
2023-05-02 13:46:52 +0200

    Tests are comleted

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -11,6 +11,7 @@ public class Exercise {
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
+        numsZeroToThree = new int[4];
         for (int i = 0; i <= 3; i++) {
             numsZeroToThree[i] = i;
         }
@@ -19,16 +20,18 @@ public class Exercise {
 
     public void stepTwo() {
         // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-        for (int i = 5; i <= 10; i++) {
-            numsFiveToTen[i] = i;
+        numsFiveToTen = new int[6];
+        for (int i = 0; i <= 5; i++) {
+            numsFiveToTen[i] = i+5;
         }
 
     }
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-        for (int i = 3; i <= 0; i--) {
-            countdown[i] = i;
+        countdown = new int[4];
+        for (int i = 0; i <= 3; i++) {
+            countdown[i] = 3-i;
         }
 
     }
