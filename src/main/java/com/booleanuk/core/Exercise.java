cb8fd2ddfb0a4c3be9e27d1373caebe576bdd1f5
Alicja Bobko <alicja.bobko@talan.com>
2023-07-06 13:38:28 +0200

    4-5 tests pass

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -41,14 +41,20 @@ public class Exercise {
 
     public boolean stepFour(int num) {
         // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array
-
+        for (int favouriteNumber : favouriteNumbers) {
+            if (favouriteNumber == num) {
+                return true;
+            }
+        }
 
         return false;
     }
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
+        for (String myHobby : myHobbies){
+            if (myHobby.equals(hobby)) return true;
+        }
 
         return false;
     }
