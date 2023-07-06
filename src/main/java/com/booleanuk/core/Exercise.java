fe6f43f44d2630ce0e0d77a369b512fb4947753f
Michal Siek <michal.siek21@gmail.com>
2023-07-06 14:42:51 +0200

    Exercise 1.1, 1.3, 1.5, Tests passed

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -10,9 +10,11 @@ public class Exercise {
     public String[] myHobbies = {"Fishing", "Language learning", "Skydiving", "Procrastinating"};
 
     public void stepOne() {
+        numsZeroToThree = new int[4];
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
-
+        for (int i =0; i<4; i++){
+            numsZeroToThree[i] = i;
+        }
     }
 
     public void stepTwo() {
@@ -23,8 +25,10 @@ public class Exercise {
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
-
+        countdown= new int[4];
+        for (int i =3; i>=0;i=i-1){
+            countdown[i]=Math.abs(i-3);
+        }
     }
 
     public boolean stepFour(int num) {
@@ -37,9 +41,12 @@ public class Exercise {
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
-
-
-        return false;
+        boolean response = false;
+        for (int i =0; i<myHobbies.length;i++){
+            if (myHobbies[i].contains(hobby)){
+                response = true;
+            }
+        }
+        return response;
     }
 }
