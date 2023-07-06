8ba0c2b490fa80d114da91310d2e889d28cc7102
Damian Gurdak <damian.gurdak@talan.com>
2023-07-06 13:08:43 +0200

    1-3 done

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -1,9 +1,9 @@
 package com.booleanuk.core;
 
 public class Exercise {
-    public int[] numsZeroToThree;
-    public int[] numsFiveToTen;
-    public int[] countdown;
+    public int[] numsZeroToThree = new int[4];
+    public int[] numsFiveToTen = new int[6];
+    public int[] countdown = new int[4];
 
     public int[] favouriteNumbers = {1, 2, 4, 5, 7, 8, 10};
 
@@ -12,17 +12,29 @@ public class Exercise {
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
 
+        for (int i = 0; i < 4; i++) {
+            numsZeroToThree[i] = i;
+        }
+
 
     }
 
     public void stepTwo() {
         // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
 
+        for (int i = 0; i < 6; i++) {
+            numsFiveToTen[i] = i + 5;
+        }
 
     }
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+
+
+        for (int i = 3; i >= 0; i--) {
+            countdown[3-i] = i;
+        }
 
 
     }
@@ -31,13 +43,11 @@ public class Exercise {
         // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array
 
 
-
         return false;
     }
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
 
 
         return false;
