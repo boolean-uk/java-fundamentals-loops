720fa5304971ea6a5bc9b6b06d5a45593c2ae034
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-06 13:04:23 +0200

    odd tasks done

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -12,6 +12,11 @@ public class Exercise {
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
 
+        numsZeroToThree = new int[4];
+
+        for(int i = 0; i < 4; i++){
+            numsZeroToThree[i] = i;
+        }
 
     }
 
@@ -25,7 +30,11 @@ public class Exercise {
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+        countdown = new int[4];
 
+        for(int i = 0; i < 4; i++){
+            countdown[i] = 3 - i;
+        }
 
     }
 
@@ -43,9 +52,11 @@ public class Exercise {
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
-
-
+        for (String element : myHobbies){
+            if(element.equals(hobby)){
+                return true;
+            }
+        }
         return false;
     }
 }
